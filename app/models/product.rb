bb0a2488491813abdad3fc0dfec6e2ac0c0ab13b
TheMoniulla <skrobis.monika@gmail.com>
2015-12-26 15:13:07 +0100

    validate format for url

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,14 @@
 class Product < ActiveRecord::Base
   has_many :order_items, inverse_of: :product
 
+  before_validation :clean_url
+
   validates :name, :url, presence: true, uniqueness: true
+  validates :url, format: {with: /\Ahttp:\/\/www.sfd.pl\/sklep\/[\w\-.]+/}
+
+  private
+
+  def clean_url
+    self.url = url.split('#').first
+  end
 end
