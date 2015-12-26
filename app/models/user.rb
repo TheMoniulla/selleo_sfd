22f7108121e05d2be3899200917147a997ebf3f1
TheMoniulla <skrobis.monika@gmail.com>
2015-12-26 15:55:08 +0100

    add gem rolify

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,17 @@
 class User < ActiveRecord::Base
+  rolify
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :order_items, inverse_of: :user
+
+  def admin?
+    has_role?(:admin)
+  end
+
+  def orderer?
+    has_role?(:orderer)
+  end
 end
