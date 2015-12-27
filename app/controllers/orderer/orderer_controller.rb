4379080ba29cbf51fbd580aa806c8529e7610219
TheMoniulla <skrobis.monika@gmail.com>
2015-12-27 14:55:48 +0100

    there can by only one open order

--- app/controllers/orderer/orderer_controller.rb
+++ app/controllers/orderer/orderer_controller.rb
@@ -1,9 +1,14 @@
 class Orderer::OrdererController < ApplicationController
   before_action :check_orderer
+  before_action :create_open_order
 
   private
 
   def check_orderer
     redirect_to root_path, alert: 'You have to be logged in as orderer to see this page.' unless current_user && current_user.orderer?
+  end
+
+  def create_open_order
+    Order.create(state: 'open') if Order.where(state: 'open').empty?
   end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,13 @@ class Order < ActiveRecord::Base
   has_many :order_items, inverse_of: :order
 
   validates :state, presence: true, inclusion: {in: %w(open close)}
+  validate :only_one_open_order_can_exist
+
+  private
+
+  def only_one_open_order_can_exist
+    if Order.where(state: 'open').count > 0
+      errors.add(:state, 'there can be only one open order')
+    end
+  end
 end
